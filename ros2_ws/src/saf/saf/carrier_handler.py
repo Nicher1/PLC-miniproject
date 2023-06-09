67a3e8c709d16e436c060e541faed001d3b4a208
Hans Henrik Dalgaard <68589761+hh4000@users.noreply.github.com>
2023-06-09 11:23:44 +0200

    Update carrier_handler.py
    Add comments
    fix missing parantheses

--- ros2_ws/src/saf/saf/carrier_handler.py
+++ ros2_ws/src/saf/saf/carrier_handler.py
@@ -6,30 +6,34 @@ from saf_msgs.msg import Carrier
 class CarrierHandler(Node):
 
     def __init__(self): 
-
+        """
+        Initializes carrier handler node. subscribes to carrier_info topic
+        """
+        #Initialize node
         super().__init__('carrier_handler')
+        # define subscription
         self.subscription = self.create_subscription(
-            Carrier,
-            'carrier_info',
-            self.carrier_info_callback,10
+            Carrier, # msg type
+            'carrier_info', # topic name
+            self.carrier_info_callback,10 # callback function (whenever topic is updated)
         )
-        self.subscription
+        self.subscription # prevent unused variable warning
         
 
-    def carrier_info_callback(self,msg):
+    def carrier_info_callback(self,msg): # callback function
         print_string = f"\nRecieved Carrier Info:\n   Carrier ID: {msg.carry_id}\n   Station ID: {msg.station_id}\n   Timestamp {msg.hour}:{msg.minute}:{msg.second}"
-        self.get_logger().info(print_string)
+        self.get_logger().info(print_string) # log information
 
 def main(args=None):
     rclpy.init(args=args)
-
+    # define carrier handler and spin
     carrier_handler = CarrierHandler()
-    rclpy.spin(carrier_handler)
+    rclpy.spin(carrier_handler) # essentially keeps the subscriber running
 
 
     #Destroy node when finished
     carrier_handler.destroy_node()
-    rclpy.shutdown
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
